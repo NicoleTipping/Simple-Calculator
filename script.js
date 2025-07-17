efbb01edaad86433ce9aba4c73fdfb21c1cf87fd
NicoleTipping <nacayard@gmail.com>
2025-07-17 12:47:09 -0500

    add operate function and button click events

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
-const displayButton = document.getElementById('display');
-const operators = document.getElementsByClassName('operator');
-const numberButtons = document.getElementsByClassName('number');
-const equalButton = document.getElementById('equal');
-const clearButton = document.getElementById('C');
+let display = document.querySelector('.display');
+let operatorButton = document.querySelector('.operator');
+let numberButton = document.querySelector('.number');
+let equalButton = document.querySelector('.equal');
+let clearButton = document.querySelector('#C');
 
+let num1;
+let num2;
+let operator;
+
+num1 = numberButton.addEventListener('click', (e) => {
+    e.target.value;
+});
+
+num2 = numberButton.addEventListener('click', (e) => {
+    e.target.value;
+});
+
+operator = operatorButton.addEventListener('click', (e) => {
+    e.target.value;
+});
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function subtract(num1, num2) {
+//     return num1 - num2;
+// }
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// function divide(num1, num2) {
+//     return num1 / num2;
+// }
+
+function operate(num1, num2, operator) {
+ switch (operator) {
+        case "+":
+            return num1 + num2;
+            break;
+        case "-":
+            return num1 - num2;
+            break;
+        case "*":
+            return num1 * num2;
+            break;
+        case "/":
+            return num1 / num2;
+            break;
+    };
+}
