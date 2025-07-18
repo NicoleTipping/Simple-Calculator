78a2740e9da690658bcbd3466d95ac556f743a0c
NicoleTipping <nacayard@gmail.com>
2025-07-18 12:19:23 -0500

    starting over with clean script.js

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
-let display = document.querySelector('.display');
-let operatorButton = document.querySelector('.operator');
-let numberButton = document.querySelector('.number');
-let equalButton = document.querySelector('.equal');
+let display = document.querySelector('#display');
+let operatorButtons = document.querySelectorAll('.operator');
+let numberButtons = document.querySelectorAll('.number');
+let equalButton = document.querySelector('#equal');
 let clearButton = document.querySelector('#C');
 
-let num1;
-let num2;
-let operator;
+let num1
+let num2
+let op
+let value
 
-num1 = numberButton.addEventListener('click', (e) => {
-    e.target.value;
-});
 
-num2 = numberButton.addEventListener('click', (e) => {
-    e.target.value;
-});
-
-operator = operatorButton.addEventListener('click', (e) => {
-    e.target.value;
-});
-
-// function add(num1, num2) {
-//     return num1 + num2;
-// }
-
-// function subtract(num1, num2) {
-//     return num1 - num2;
-// }
-
-// function multiply(num1, num2) {
-//     return num1 * num2;
-// }
-
-// function divide(num1, num2) {
-//     return num1 / num2;
-// }
-
-function operate(num1, num2, operator) {
- switch (operator) {
+function operate(num1, num2) {
+    switch(op) {
         case "+":
-            return num1 + num2;
+            return num1 + num2
             break;
         case "-":
-            return num1 - num2;
+            return num1 - num2
             break;
         case "*":
-            return num1 * num2;
+            return num1 * num2
             break;
         case "/":
-            return num1 / num2;
+            return num1 / num2
             break;
-    };
+    }
+};
+
+
+numberButtons.forEach((numberButton) => {numberButton.addEventListener('click', handleButClick)})
+
+operatorButtons.forEach((operatorButton) => {operatorButton.addEventListener('click', handleOpClick)})
+
+function handleButClick(e) {
+    value = e.target.textContent;
+}
+
+function handleOpClick(e) {
+    op = e.target.textContent
 }
