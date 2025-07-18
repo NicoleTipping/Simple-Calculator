8fa6a52baeecdfd870baa60e0632ff0d764669ef
NicoleTipping <nacayard@gmail.com>
2025-07-18 13:09:02 -0500

    add Event Listeners for Operations and Button click and displays on calculator

--- script.js
+++ script.js
@@ -4,11 +4,35 @@ let numberButtons = document.querySelectorAll('.number');
 let equalButton = document.querySelector('#equal');
 let clearButton = document.querySelector('#C');
 
-let num1
-let num2
+let num1 = ""
+let num2 = ""
 let op
-let value
+let turn = true
 
+numberButtons.forEach((numberButton) => {numberButton.addEventListener('click', handleButClick)})
+
+operatorButtons.forEach((operatorButton) => {operatorButton.addEventListener('click', handleOpClick)})
+
+function handleButClick(e) {
+    if(turn){
+        num1 += e.target.textContent
+        display.textContent = num1
+    } else {
+        num2 = e.target.textContent
+        display.textContent += num2
+    }
+}
+
+function handleOpClick(e) {
+    turn = false
+    op = e.target.textContent
+    display.textContent += op
+}
+
+clearButton.addEventListener('click', () => {
+    turn = true;
+    display.textContent = 0;
+})
 
 function operate(num1, num2) {
     switch(op) {
@@ -25,17 +49,4 @@ function operate(num1, num2) {
             return num1 / num2
             break;
     }
-};
-
-
-numberButtons.forEach((numberButton) => {numberButton.addEventListener('click', handleButClick)})
-
-operatorButtons.forEach((operatorButton) => {operatorButton.addEventListener('click', handleOpClick)})
-
-function handleButClick(e) {
-    value = e.target.textContent;
-}
-
-function handleOpClick(e) {
-    op = e.target.textContent
 }
